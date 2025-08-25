3cbf8b4b7d11410bb7318393bbef1519c44eda3a
alaa shaher <alaashaher12@gmail.com>
2025-08-25 16:32:01 +0300

    skills section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/Home/HeroSection";
+import SkillsSection from "@/components/Home/SkillsSection";
 
 export default function Home() {
   return (
     <div >
       <HeroSection />
+      <SkillsSection />
     </div>
   );
 }
